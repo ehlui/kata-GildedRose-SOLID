11f54d06c75c390864cf0c8fcfd0e91a9f2bafc9
ehlui <86680816+ehlui@users.noreply.github.com>
2023-02-11 23:55:23 +0100

    Adding all tests coverage before refactoring

---
+++ README.md
@@ -0,0 +1,31 @@
+# Gilded Rose starting position in Java
+
+## Run the Text Fixture from Command-Line
+
+```
+./gradlew -q text
+```
+
+### Specify Number of Days
+
+For e.g. 10 days:
+
+```
+./gradlew -q text --args 10
+```
+
+---
+
+## Luis comments
+
+- This is a repo extracted from [Emily Bache github](https://github.com/emilybache/GildedRose-Refactoring-Kata)
+  which I will be using for practising refactoring with Java and testing.
+
+- Video for helping -> [YT official creator](https://www.youtube.com/watch?v=zyM2Ep28ED8&ab_channel=EficodePraqma)
+
+### Testing
+how well we can test our code?
+1. **coverage tests** (try to have a full coverage on our business logic or use cases)
+2. **mutation tests** (change code after tests to check if fails which means they are ok before a refactoring)
+    - We create a "mutation" on a specific line of code to check if we break our test
+    - We create a "mutation" on a specific line of code to check if we break our test
---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,37 @@
+package com.gildedrose;
+
+import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
+import org.approvaltests.core.Options;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GildedRoseTest {
+
+    @Test
+    void when_update_quality() {
+        // String itemString = doUpdateQuality("foo", 0, 0);
+        //Approvals.verify(itemString);
+        CombinationApprovals.verifyAllCombinations(
+            this::doUpdateQuality,
+            // If we only have an if we can have a greater coverage on its opposite path (example of "Sulfuras, Hand of Ragnaros" name)
+            new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
+            new Integer[]{-1, 0, 2, 6, 11},
+            new Integer[]{0, 1, 49, 50}
+        );
+
+    }
+
+    private String doUpdateQuality(String name, int sellIn, int quality) {
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        //assertEquals("foo", app.items[0].name);
+
+        // Comparing from the txt
+        String itemString = app.items[0].toString();
+        return itemString;
+    }
+
+}
